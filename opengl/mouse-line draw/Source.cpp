e48cc8f3dacc39630e3c8aa3ed40f6cf1541e708
raneemwael2002 <66146343+raneemwael2002@users.noreply.github.com>
2024-11-27 22:28:19 +0200

    Update Source.cpp

--- opengl/mouse-line draw/Source.cpp
+++ opengl/mouse-line draw/Source.cpp
@@ -2,202 +2,216 @@
 #include <gl/glew/glew.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
-#include<gl\glm\glm.hpp>
+#include <gl/glm/glm.hpp>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 using namespace glm;
 
 enum DrawingMode
 {
-	Points,
-	Lines,
-	FilledTriangle
+    Points,
+    Lines,
+    FilledTriangle,
+    DynamicLines 
 };
 
 struct Vertex
 {
-	vec3 Position;
-	vec3 COlor;
+    vec3 Position;
+    vec3 Color;
 };
 
 GLuint InitShader(const char* vertex_shader_file_name, const char* fragment_shader_file_name);
 
 const GLint WIDTH = 600, HEIGHT = 600;
-GLuint VBO, BasiceprogramId;
-DrawingMode Current_DrawingMode = DrawingMode::FilledTriangle;
+GLuint VBO, DynamicLineVBO, BasiceprogramId;
+DrawingMode Current_DrawingMode = DrawingMode::DynamicLines;
+
+std::vector<GLfloat> lineVertices; 
+bool isDrawing = false;
 
 
-void DrawTriangleFan()
+
+void CreateDynamicLines()
 {
-	const int numSegments = 16;
-	const float radius = 1.0f;
-	const float PI = 3.14159265f;
+    glGenBuffers(1, &DynamicLineVBO);
 
-	// Each vertex has x, y (2 floats) + r, g, b (3 floats)
-	const int floatsPerVertex = 5;
-	GLfloat vertices[(numSegments + 2) * floatsPerVertex];
+ 
+    glBindBuffer(GL_ARRAY_BUFFER, DynamicLineVBO);
+    glBufferData(GL_ARRAY_BUFFER, 10000 * sizeof(GLfloat), NULL, GL_DYNAMIC_DRAW); 
 
-	// Center vertex (for the fan)
-	vertices[0] = 0.0f;  // x
-	vertices[1] = 0.0f;  // y
-	vertices[2] = 0.9098f; // r
-	vertices[3] = 0.7372f; // g
-	vertices[4] = 0.7254f; // b
+ 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+}
 
-
-	for (int i = 0; i < numSegments + 1; ++i)
-	{
-		float angle = 2.0f * PI * i / numSegments;
-
-		// Position
-		vertices[(i + 1) * floatsPerVertex] = radius * cos(angle);     // x
-		vertices[(i + 1) * floatsPerVertex + 1] = radius * sin(angle); // y
-
-		// Color (assign based on segments)
-		if (i < numSegments / 3) {
-			vertices[(i + 1) * floatsPerVertex + 2] = 0.9098f; // r
-			vertices[(i + 1) * floatsPerVertex + 3] = 0.7372f; // g
-			vertices[(i + 1) * floatsPerVertex + 4] = 0.7254f; // b
-		}
-		else if (i < 2 * numSegments / 3) {
-			vertices[(i + 1) * floatsPerVertex + 2] = 0.2627f; // r
-			vertices[(i + 1) * floatsPerVertex + 3] = 0.1803f; // g
-			vertices[(i + 1) * floatsPerVertex + 4] = 0.3294f; // b
-		}
-		else {
-			vertices[(i + 1) * floatsPerVertex + 2] = 0.6823f; // r
-			vertices[(i + 1) * floatsPerVertex + 3] = 0.2666f; // g
-			vertices[(i + 1) * floatsPerVertex + 4] = 0.3529f; // b
-		}
-	}
-
-
-	// Create and bind buffer object
-	GLuint VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// Set up vertex attribute pointers
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, floatsPerVertex * sizeof(GLfloat), (void*)0); // Position
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, floatsPerVertex * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat))); // Color
-	glEnableVertexAttribArray(1);
+void UpdateDynamicLineBuffer()
+{
+    if (!lineVertices.empty())
+    {
+        
+        glBindBuffer(GL_ARRAY_BUFFER, DynamicLineVBO);
+      //  glBufferData(GL_ARRAY_BUFFER, lineVertices.size() * sizeof(vec3), nullptr, GL_DYNAMIC_DRAW);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, lineVertices.size() * sizeof(GLfloat), lineVertices.data());
+    }
+    
 }
 
 
 
-void CompileShader(const char* vertex_shader_file_name, const char* fragment_shader_file_namering, GLuint& programId)
+void CompileShader(const char* vertex_shader_file_name, const char* fragment_shader_file_name, GLuint& programId)
 {
-	programId = InitShader(vertex_shader_file_name, fragment_shader_file_namering);
-	glUseProgram(programId);
+    programId = InitShader(vertex_shader_file_name, fragment_shader_file_name);
+    glUseProgram(programId);
 }
 
 int Init()
 {
-	GLenum err = glewInit();
-	if (err != GLEW_OK)
-	{
-		cout << "Error";
-		getchar();
-		return 1;
-	}
-	else
-	{
-		if (GLEW_VERSION_3_0)
-			cout << "Driver support OpenGL 3.0\nDetails:\n";
-	}
-	cout << "\tUsing glew " << glewGetString(GLEW_VERSION) << endl;
-	cout << "\tVendor: " << glGetString(GL_VENDOR) << endl;
-	cout << "\tRenderer: " << glGetString(GL_RENDERER) << endl;
-	cout << "\tVersion: " << glGetString(GL_VERSION) << endl;
-	cout << "\tGLSL:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    GLenum err = glewInit();
+    if (err != GLEW_OK)
+    {
+        cout << "Error";
+        getchar();
+        return 1;
+    }
+    else
+    {
+        if (GLEW_VERSION_3_0)
+            cout << "Driver support OpenGL 3.0\nDetails:\n";
+    }
+    cout << "\tUsing glew " << glewGetString(GLEW_VERSION) << endl;
+    cout << "\tVendor: " << glGetString(GL_VENDOR) << endl;
+    cout << "\tRenderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "\tVersion: " << glGetString(GL_VERSION) << endl;
+    cout << "\tGLSL:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
 
-	CompileShader("VS.glsl", "FS.glsl", BasiceprogramId);
-	DrawTriangleFan();
-	
-	glClearColor(0.9647f, 0.9372f, 0.7411f, 1);
+    CompileShader("VS.glsl", "FS.glsl", BasiceprogramId);
+    CreateDynamicLines(); 
+    glClearColor(0.9647f, 0.9372f, 0.7411f, 1);
 
-	return 0;
+    return 0;
 }
 
 void Render()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	switch (Current_DrawingMode)
-	{
-	case Points:
-		glPointSize(10);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-		break;
-	case Lines:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		break;
-	case FilledTriangle:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		break;
-	default:
-		break;
-	}
-
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 18);
+    switch (Current_DrawingMode)
+    {
+    case Points:
+        glPointSize(10);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+        glDrawArrays(GL_POINTS, 0, 3);
+        break;
+    case Lines:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        break;
+    case FilledTriangle:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        break;
+    case DynamicLines:
+        glLineWidth(5.0f);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glBindBuffer(GL_ARRAY_BUFFER, DynamicLineVBO);
+        //glDrawArrays(GL_LINES, 0, lineVertices.size() /3);
+      glDrawArrays(GL_LINE_STRIP, 0, lineVertices.size()/3);
+        break;
+    default:
+        break;
+    }
 }
 
 float theta = 0;
 void Update()
 {
-	// add all tick code
-	theta += 0.0001f;
+    // Add all tick code
+    theta += 0.0001f;
 
-	GLuint Theta_Location = glGetUniformLocation(BasiceprogramId, "theta");
-	glUniform1f(Theta_Location, theta);
+    GLuint Theta_Location = glGetUniformLocation(BasiceprogramId, "theta");
+    glUniform1f(Theta_Location, theta);
+
+    if (Current_DrawingMode == DynamicLines)
+        UpdateDynamicLineBuffer(); 
 }
 
 int main()
 {
-	sf::ContextSettings context;
-	context.depthBits = 24;
-	sf::Window window(sf::VideoMode(WIDTH, HEIGHT), " circle made of triangle fan", sf::Style::Close, context);
+    sf::ContextSettings context;
+    context.depthBits = 24;
+    sf::Window window(sf::VideoMode(WIDTH, HEIGHT), "SFML works!", sf::Style::Close, context);
 
-	if (Init()) return 1;
+    if (Init()) return 1;
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			switch (event.type)
-			{
-			case sf::Event::Closed:
-			{
-				window.close();
-				break;
-			}
-			case sf::Event::KeyPressed:
-			{
-				if (event.key.code == sf::Keyboard::Num1)
-				{
-					Current_DrawingMode = DrawingMode::Points;
-				}
-				if (event.key.code == sf::Keyboard::Num2)
-				{
-					Current_DrawingMode = DrawingMode::Lines;
-				}
-				if (event.key.code == sf::Keyboard::Num3)
-				{
-					Current_DrawingMode = DrawingMode::FilledTriangle;
-				}
-				break;
-			}
-			}
-		}
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
 
-		Update();
-		Render();
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Num1)
+                {
+                    Current_DrawingMode = DrawingMode::Points;
+                }
+                if (event.key.code == sf::Keyboard::Num2)
+                {
+                    Current_DrawingMode = DrawingMode::Lines;
+                }
+                if (event.key.code == sf::Keyboard::Num3)
+                {
+                    Current_DrawingMode = DrawingMode::FilledTriangle;
+                }
+                if (event.key.code == sf::Keyboard::Num4)
+                {
+                    Current_DrawingMode = DrawingMode::DynamicLines;
+                }
+                break;
 
-		window.display();
-	}
-	return 0;
+            case sf::Event::MouseButtonPressed:
+                if (Current_DrawingMode == DynamicLines && event.mouseButton.button == sf::Mouse::Left)
+                {
+                    isDrawing = true;
+                    float x = (event.mouseButton.x / (float)WIDTH) * 2 - 1;
+                    float y = -((event.mouseButton.y / (float)HEIGHT) * 2 - 1);
+                    lineVertices.push_back(x);
+                    lineVertices.push_back(y);
+                    lineVertices.push_back(0); 
+                }
+                break;
+
+            case sf::Event::MouseMoved:
+                if (isDrawing && Current_DrawingMode == DynamicLines)
+                {
+                    float x = (event.mouseMove.x / (float)WIDTH) * 2 - 1;
+                    float y = -((event.mouseMove.y / (float)HEIGHT) * 2 - 1);
+                    lineVertices.push_back(x);
+                    lineVertices.push_back(y);
+                    lineVertices.push_back(0); 
+                }
+                break;
+
+            case sf::Event::MouseButtonReleased:
+                if (Current_DrawingMode == DynamicLines && event.mouseButton.button == sf::Mouse::Left)
+                {
+                    isDrawing = false;
+                }
+                break;
+            }
+        }
+
+        Update();
+        Render();
+
+        window.display();
+    }
+    return 0;
 }
