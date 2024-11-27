0951d00323f5e141621f22dd20fa43015ec8d787
raneemwael2002 <66146343+raneemwael2002@users.noreply.github.com>
2024-11-27 23:03:23 +0200

    Update Source.cpp

--- opengl/sin-wave/Source.cpp
+++ opengl/sin-wave/Source.cpp
@@ -1,203 +1,162 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 #include <gl/glew/glew.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
-#include<gl\glm\glm.hpp>
 
 using namespace std;
-using namespace glm;
-
-enum DrawingMode
-{
-	Points,
-	Lines,
-	FilledTriangle
-};
-
-struct Vertex
-{
-	vec3 Position;
-	vec3 COlor;
-};
-
-GLuint InitShader(const char* vertex_shader_file_name, const char* fragment_shader_file_name);
-
 const GLint WIDTH = 600, HEIGHT = 600;
-GLuint VBO, BasiceprogramId;
-DrawingMode Current_DrawingMode = DrawingMode::FilledTriangle;
+GLuint VBO, VAO, shaderProgram;
 
-
-void DrawTriangleFan()
+void CreateSineWave()
 {
-	const int numSegments = 16;
-	const float radius = 1.0f;
-	const float PI = 3.14159265f;
+    const int numPoints = 500;
+    const float step = 2.0f / (numPoints - 1);  
+    std::vector<float> vertices; 
 
-	// Each vertex has x, y (2 floats) + r, g, b (3 floats)
-	const int floatsPerVertex = 5;
-	GLfloat vertices[(numSegments + 2) * floatsPerVertex];
+    
+    for (int i = 0; i < numPoints; i++)
+    {
+        float x = -1.0f + i * step; 
+        vertices.push_back(x);      
+        vertices.push_back(0.0f);    
+        vertices.push_back(0.0f);    
+        vertices.push_back(1.0f);    
+        vertices.push_back(1.0f);    
+        vertices.push_back(1.0f);    
+    }
 
-	// Center vertex (for the fan)
-	vertices[0] = 0.0f;  // x
-	vertices[1] = 0.0f;  // y
-	vertices[2] = 0.9098f; // r
-	vertices[3] = 0.7372f; // g
-	vertices[4] = 0.7254f; // b
+    
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
 
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
 
-	for (int i = 0; i < numSegments + 1; ++i)
-	{
-		float angle = 2.0f * PI * i / numSegments;
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-		// Position
-		vertices[(i + 1) * floatsPerVertex] = radius * cos(angle);     // x
-		vertices[(i + 1) * floatsPerVertex + 1] = radius * sin(angle); // y
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-		// Color (assign based on segments)
-		if (i < numSegments / 3) {
-			vertices[(i + 1) * floatsPerVertex + 2] = 0.9098f; // r
-			vertices[(i + 1) * floatsPerVertex + 3] = 0.7372f; // g
-			vertices[(i + 1) * floatsPerVertex + 4] = 0.7254f; // b
-		}
-		else if (i < 2 * numSegments / 3) {
-			vertices[(i + 1) * floatsPerVertex + 2] = 0.2627f; // r
-			vertices[(i + 1) * floatsPerVertex + 3] = 0.1803f; // g
-			vertices[(i + 1) * floatsPerVertex + 4] = 0.3294f; // b
-		}
-		else {
-			vertices[(i + 1) * floatsPerVertex + 2] = 0.6823f; // r
-			vertices[(i + 1) * floatsPerVertex + 3] = 0.2666f; // g
-			vertices[(i + 1) * floatsPerVertex + 4] = 0.3529f; // b
-		}
-	}
-
-
-	// Create and bind buffer object
-	GLuint VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// Set up vertex attribute pointers
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, floatsPerVertex * sizeof(GLfloat), (void*)0); // Position
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, floatsPerVertex * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat))); // Color
-	glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
 
-
-
-void CompileShader(const char* vertex_shader_file_name, const char* fragment_shader_file_namering, GLuint& programId)
+void CompileShaders()
 {
-	programId = InitShader(vertex_shader_file_name, fragment_shader_file_namering);
-	glUseProgram(programId);
+    const char* vertexShaderSource = R"(
+        #version 330
+        layout (location = 0) in vec4 vertex_position;
+        layout (location = 1) in vec4 vertex_color;
+
+        uniform float amplitude;  
+        uniform float frequency;  
+
+        out vec4 frag_color;
+
+        void main()
+        {
+            float y = amplitude * sin(frequency * vertex_position.x * 3.14159f);  
+            gl_Position = vec4(vertex_position.x, y, 0.0, 1.0);
+            frag_color = vertex_color;
+        }
+    )";
+
+    const char* fragmentShaderSource = R"(
+        #version 330
+        in vec4 frag_color;
+        out vec4 color;
+
+        void main()
+        {
+            color = frag_color;
+        }
+    )";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
 
 int Init()
 {
-	GLenum err = glewInit();
-	if (err != GLEW_OK)
-	{
-		cout << "Error";
-		getchar();
-		return 1;
-	}
-	else
-	{
-		if (GLEW_VERSION_3_0)
-			cout << "Driver support OpenGL 3.0\nDetails:\n";
-	}
-	cout << "\tUsing glew " << glewGetString(GLEW_VERSION) << endl;
-	cout << "\tVendor: " << glGetString(GL_VENDOR) << endl;
-	cout << "\tRenderer: " << glGetString(GL_RENDERER) << endl;
-	cout << "\tVersion: " << glGetString(GL_VERSION) << endl;
-	cout << "\tGLSL:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    GLenum err = glewInit();
+    if (err != GLEW_OK)
+    {
+        cout << "Error";
+        getchar();
+        return 1;
+    }
+    else
+    {
+        if (GLEW_VERSION_3_0)
+            cout << "Driver support OpenGL 3.0\nDetails:\n";
+    }
+    cout << "\tUsing glew " << glewGetString(GLEW_VERSION) << endl;
+    cout << "\tVendor: " << glGetString(GL_VENDOR) << endl;
+    cout << "\tRenderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "\tVersion: " << glGetString(GL_VERSION) << endl;
+    cout << "\tGLSL:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    std::cout << "GLEW initialized successfully" << std::endl;
+    CompileShaders();
+    CreateSineWave();
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    return 0;
 
-	CompileShader("VS.glsl", "FS.glsl", BasiceprogramId);
-	DrawTriangleFan();
-	
-	glClearColor(0.9647f, 0.9372f, 0.7411f, 1);
-
-	return 0;
 }
+
 
 void Render()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	switch (Current_DrawingMode)
-	{
-	case Points:
-		glPointSize(10);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-		break;
-	case Lines:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		break;
-	case FilledTriangle:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		break;
-	default:
-		break;
-	}
+    glUseProgram(shaderProgram);
 
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 18);
+    glUniform1f(glGetUniformLocation(shaderProgram, "amplitude"), 0.1f);  
+    glUniform1f(glGetUniformLocation(shaderProgram, "frequency"), 15.0f); 
+
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_LINE_STRIP, 0, 500); 
+    glBindVertexArray(0);
 }
 
-float theta = 0;
-void Update()
-{
-	// add all tick code
-	theta += 0.0001f;
-
-	GLuint Theta_Location = glGetUniformLocation(BasiceprogramId, "theta");
-	glUniform1f(Theta_Location, theta);
-}
 
 int main()
 {
-	sf::ContextSettings context;
-	context.depthBits = 24;
-	sf::Window window(sf::VideoMode(WIDTH, HEIGHT), " circle made of triangle fan", sf::Style::Close, context);
+    sf::ContextSettings context;
+    context.depthBits = 24;
+    sf::Window window(sf::VideoMode(WIDTH, HEIGHT), "Sine Wave", sf::Style::Close, context);
 
-	if (Init()) return 1;
+    if (Init())
+        return 1;
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			switch (event.type)
-			{
-			case sf::Event::Closed:
-			{
-				window.close();
-				break;
-			}
-			case sf::Event::KeyPressed:
-			{
-				if (event.key.code == sf::Keyboard::Num1)
-				{
-					Current_DrawingMode = DrawingMode::Points;
-				}
-				if (event.key.code == sf::Keyboard::Num2)
-				{
-					Current_DrawingMode = DrawingMode::Lines;
-				}
-				if (event.key.code == sf::Keyboard::Num3)
-				{
-					Current_DrawingMode = DrawingMode::FilledTriangle;
-				}
-				break;
-			}
-			}
-		}
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-		Update();
-		Render();
+        Render();
+        window.display();
+    }
 
-		window.display();
-	}
-	return 0;
+    return 0;
 }
