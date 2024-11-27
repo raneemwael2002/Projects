9b5916997783cdb86c52d019ce990bdd66f4af23
raneemwael2002 <66146343+raneemwael2002@users.noreply.github.com>
2024-11-27 21:15:58 +0200

    Add files via upload

---
+++ opengl/Sierpinski Triangle/FS.glsl
@@ -0,0 +1,8 @@
+#version 330
+
+in vec4 frag_color;
+
+void main()
+{
+	gl_FragColor = frag_color;
+}
---
+++ opengl/Sierpinski Triangle/VS.glsl
@@ -0,0 +1,20 @@
+#version 330
+
+layout (location = 0) in vec4 vertex_position;
+layout (location = 1) in vec4 vertex_color;
+
+uniform float theta;
+
+out vec4 frag_color;
+void main()
+{
+	//gl_Position = vertex_position;
+
+	float x = vertex_position.x * cos(theta) + vertex_position.y * sin(theta);
+	float y = -vertex_position.x * sin(theta) + vertex_position.y * cos(theta);
+	
+	gl_Position = vec4(x/2,y/2,vertex_position.z,1);
+
+	//frag_color = vertex_position;
+	frag_color = vertex_color;
+}
