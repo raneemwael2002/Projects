c0506cea2bb7306441412f501772f2c421665d2d
raneemwael2002 <66146343+raneemwael2002@users.noreply.github.com>
2024-11-27 22:29:39 +0200

    Update VS.glsl

--- opengl/mouse-line draw/VS.glsl
+++ opengl/mouse-line draw/VS.glsl
@@ -8,12 +8,12 @@ uniform float theta;
 out vec4 frag_color;
 void main()
 {
-	//gl_Position = vertex_position;
+	gl_Position = vertex_position;
 
-	float x = vertex_position.x * cos(theta) + vertex_position.y * sin(theta);
-	float y = -vertex_position.x * sin(theta) + vertex_position.y * cos(theta);
+//	float x = vertex_position.x * cos(theta) + vertex_position.y * sin(theta);
+//	float y = -vertex_position.x * sin(theta) + vertex_position.y * cos(theta);
 	
-	gl_Position = vec4(x/2,y/2,vertex_position.z,1);
+//	gl_Position = vec4(x/2,y/2,vertex_position.z,1);
 
 	//frag_color = vertex_position;
 	frag_color = vertex_color;
